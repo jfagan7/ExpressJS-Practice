a6812380f894bc0c6620bf11167a4a1cc71191cf
jfagan7 <jfagan6409@bths.edu>
2017-12-31 20:54:38 -0500

    Added the ejs module as well as nodemon

--- CustomerApp/app.js
+++ CustomerApp/app.js
@@ -1,12 +1,26 @@
-console.log('Hello World');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 //added comments
 var app = express();
+/*
+var logger = function(req, res, next)
+{
+  console.log('Logging...');
+  next();
+}
+app.use(logger);
+*/
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+//Set Static Path
+app.use(express.static(path.join(__dirname,'public')))
+
 app.get('/',function (req,res) {
-  res.send('Hello World');
+  res.send('Hello');
 });
+
 app.listen(3000, function () {
   console.log('Server started on port 3000...');
 })
