86246b8cd6ed4a1e9b0ad5715e21564b0d4258a3
jfagan7 <jfagan6409@bths.edu>
2018-01-01 10:43:17 -0500

    Added the express-validator module

--- CustomerApp/app.js
+++ CustomerApp/app.js
@@ -11,15 +11,54 @@ var logger = function(req, res, next)
 }
 app.use(logger);
 */
+
+//View Engine
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
 //Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 //Set Static Path
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
+
+var users  = [
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@gmail.com'
+  },
+  {
+    id: 2,
+    first_name: 'Bob',
+    last_name: 'Smith',
+    email: 'bobsmith@gmail.com'
+  },
+  {
+    id: 3,
+    first_name: 'Jill',
+    last_name: 'Jackson',
+    email: 'jilljackson@gmail.com'
+  }
+]
 
 app.get('/',function (req,res) {
-  res.send('Hello');
+  res.render('index',{
+    title: 'Customer',
+    users: users
+  });
 });
+
+app.post('/users/add', function(req,res)
+{
+  console.log(req.body);
+  var newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  };
+})
 
 app.listen(3000, function () {
   console.log('Server started on port 3000...');
